3b91049683fcd97811f158343164738c8dae90c8
OleksandrMakarov <alexander.vmv@gmail.com>
2023-08-08 16:49:01 +0200

    add output to manage Webhook

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
-output "function_url" {
-  description = "Function url"
-  value = aws_lambda_function_url.lambda_function_url.function_url
+output "setWebhook_url" {
+  description = "URL to set Webhook"
+  value = "https://api.telegram.org/bot${var.telegram_token}/setWebhook?url=${aws_lambda_function_url.lambda_function_url.function_url}"
+}
+
+output "deleteWebhook_url" {
+  description = "URL to delete Webhook"
+  value = "https://api.telegram.org/bot${var.telegram_token}/deleteWebhook"
 }
