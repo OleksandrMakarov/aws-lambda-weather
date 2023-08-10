ed95e2e208dc5ce7bf26ecd053f121aecc4e54b8
OleksandrMakarov <alexander.vmv@gmail.com>
2023-08-10 11:23:55 +0200

    change gitignore

---
+++ terraform/telegram_module/variables.tf
@@ -0,0 +1,28 @@
+variable "telegram_bot_webhook_zip_path" {
+  type = string
+}
+
+variable "iam_role_arn" {
+  type = string
+}
+
+variable "iam_role_name" {
+  type = string
+}
+
+variable "weather_notification_lambda_name" {
+  type = string
+}
+
+variable "table_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
---
+++ terraform/weather_module/variables.tf
@@ -0,0 +1,28 @@
+variable "weather_notification_zip_path" {
+  type = string
+}
+
+variable "iam_role_arn" {
+  type = string
+}
+
+variable "iam_role_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "aws_secret_name" {
+  type = string
+}
+
+variable "table_name" {
+  type = string
+}
+
